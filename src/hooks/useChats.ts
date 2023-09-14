ece671f750f0d76c8162f5a3061ccb3f3841dede
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-09-13 22:23:52 -0300

    removed: unnecessary code

--- src/hooks/useChats.ts
+++ src/hooks/useChats.ts
@@ -12,7 +12,7 @@ import {
   where,
 } from "firebase/firestore";
 import { database } from "../services/firebase";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAuth } from "./useAuth";
 
 type IUserData = {
@@ -57,6 +57,25 @@ function useChats() {
   const [chats, setChats] = useState<IChat[]>([]);
   const { user } = useAuth();
 
+  const getPrivateUserData = useCallback(
+    (chatData: IChatData) => {
+      if (chatData.isAGroup) return chatData;
+
+      const members = new Map(Object.entries(chatData.members));
+
+      const privateChatKey = [...members.keys()].find(
+        (key) => key !== user!.uid,
+      );
+
+      const { photoURL, displayName } = members.get(
+        privateChatKey!,
+      ) as IUserData;
+
+      return { ...chatData, name: displayName, photoURL };
+    },
+    [user],
+  );
+
   useEffect(() => {
     if (!user) return;
     const q = query(
@@ -64,11 +83,19 @@ function useChats() {
       where(`members.${user.uid}`, "!=", ""),
     ).withConverter(converter);
 
+    // getDocs(q).then(response => {
+    //   response.forEach(doc => {
+
+    //   });
+    // }).catch(err => {
+    //   console.error(err);
+    // });
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const chatsData: IChat[] = [];
       querySnapshot.forEach((doc) => {
         chatsData.push({
-          ...doc.data(),
+          ...getPrivateUserData(doc.data()),
           id: doc.id,
         });
       });
@@ -78,30 +105,30 @@ function useChats() {
     return () => {
       unsubscribe();
     };
-  }, [user]);
+  }, [user, getPrivateUserData]);
   const chatRef = collection(database, "chat");
 
   async function findChatByUsers(users: Map<string, IUserData>) {
     // console.log(users);
     const ref: QueryConstraint[] = [];
     users.forEach((value, key) => {
-      ref.push(where(`members.${key}`, "==", true));
+      ref.push(where(`members.${key}`, "==", value));
     });
-    ref.push(where("type", "==", 1));
+    // ref.push(where("isAGroup", "==", false));
     const q = query(chatRef, ...ref);
     const docSnapshot = await getDocs(q);
-    console.log(docSnapshot.docs);
-    docSnapshot.forEach((doc) => {
-      console.log(doc.id, doc.data());
-    });
+    // console.log(docSnapshot.docs);
+    // docSnapshot.forEach((doc) => {
+    // console.log(doc.id, doc.data());
+    // });
 
     return docSnapshot.docs.length > 0;
   }
 
   async function createAChat(data: Partial<IChat>) {
-    if (data.isAGroup) {
+    if (!data.isAGroup) {
       const response = await findChatByUsers(data.members!);
-      // console.log(response);
+      console.log(response);
       if (response) throw new Error("Não foi possível criar a conversa!");
     }
     // console.log(data);
